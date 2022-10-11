3977b5bde05670d1527f4842fbd31a6abda958e7
diyorkeremas <108538560+diyorkeremas@users.noreply.github.com>
2022-10-11 21:49:25 +0500

    finiwed

---
+++ js/main.js
@@ -0,0 +1,16 @@
+alert ('Royhatni Toldiring');
+
+
+let ism ='diyorjon';
+console.log(ism);
+
+
+let kasb = 'dasturchi';
+console.log('familya');
+
+
+let jins = 'erkak';
+console.log('jins');
+
+let yosh = '16';
+console.log('yosh');
